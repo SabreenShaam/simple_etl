4b1a29a83e495a12c51a19f05fa2230c67e94ace
SabreenShaam <hsuberin@gmail.com>
2024-12-04 14:45:24 +0530

    added a sample ETL app with a DAG

---
+++ etl/etl_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    'mysql_etl_dag',  # DAG name
+    default_args=default_args,
+    description='A simple ETL DAG',
+    schedule_interval=timedelta(minutes=5),
+    start_date=datetime(2024, 8, 8),
+    catchup=False,
+)
+
+run_etl = BashOperator(
+    task_id='run_etl',
+    bash_command='bash /home/sabreen/work/Projects/Airflow/airflow_demo/wrapper_script.sh ',
+    # give a space after the path
+    dag=dag,
+)
---
+++ etl/wrapper_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /home/sabreen/work/Projects/Airflow/airflow_demo/etl_script.py
